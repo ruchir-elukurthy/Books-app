175b1028b26500ba00de0328d354fd9aad80f646
Ruchir <ruchir@pdx.edu>
2020-08-25 23:48:19 -0700

    search button, add the required functionality that was planned for it to do

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import Book from "./components/Book";
 
 export class App extends Component {
   state = {
     book: [],
-    loading: true,
   };
 
-  async componentDidMount() {
+  getBook = async (e) => {
+    e.preventDefault();
     const response = await fetch(
       "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=fVIsBGGuPGFTzZ4FqIWLZYtHCGqGnYU3"
     );
@@ -14,26 +15,30 @@ export class App extends Component {
     console.log(data);
     this.setState({
       book: data.results.books,
-      loading: false,
     });
-  }
+  };
 
   render() {
-    if (this.state.loading) {
-      return <div>LOADING...</div>;
-    }
-    if (!this.state.book.length) {
-      return <div>No book available</div>;
-    }
+    // if (this.state.loading === true) {
+    //   return <div>LOADING...</div>;
+    // }
+    // if (!this.state.book.length) {
+    //   return <div>No book available</div>;
+    // }
     return (
-      <div>
+      <div className="App">
+        <header className="Appheader">
+          <h1 className="App-title">Book Search</h1>
+        </header>
+        <Book getBook={this.getBook} />
         {this.state.book.map((novel) => (
           <div key={novel.title}>
-            <div>{novel.title}</div>
-            <div>{novel.author}</div>
-            <div>{novel.description}</div>
-            <div>{novel.book_image}</div>
-            <div>{novel.amazon_product_url}</div>
+            <div>Book name: {novel.title}</div>
+            <div>Author: {novel.author}</div>
+            <div>Description: {novel.description}</div>
+            <img src={novel.book_image} alt={novel.title} />
+            <br />
+            <a href={novel.amazon_product_url}>Check book at Amazon</a>
             <br />
             <br />
             <br />
